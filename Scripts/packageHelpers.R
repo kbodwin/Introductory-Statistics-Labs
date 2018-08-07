52c97c54ef4056acdca2cafe995b51180d673475
Kelly Bodwin <kbodwin@calpoly.edu>
2018-08-07 15:21:07 -0700

    Lab1 and PreLab1
    Updated versions for Rstudio Cloud

--- Scripts/packageHelpers.R
+++ Scripts/packageHelpers.R
@@ -1,4 +1,19 @@
-library('learnr')
-library('shiny')
-library('ggplot2')
-library('dplyr')
+if(!require(tidyverse)){
+	install.packages("tidyverse")
+	library(tidyverse)
+}
+
+if(!require(shiny)){
+	install.packages("shiny")
+	library(shiny)
+}
+
+if(!require(learnr)){
+	install.packages("learnr")
+	library(learnr)
+}
+
+if(!require(httpuv)){
+	install.packages("httpuv")
+	library(httpuv)
+}
