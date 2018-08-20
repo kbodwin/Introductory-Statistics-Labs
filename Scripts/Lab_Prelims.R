f7b220e45c01b5c3e26ee4d42cc5cd898680f751
Kelly Bodwin <kbodwin@calpoly.edu>
2018-08-20 14:36:31 -0700

    updated source links
    Replaced source links in labs with github locations

---
+++ Scripts/Lab_Prelims.R
@@ -0,0 +1,6 @@
+knitr::opts_chunk$set(echo = FALSE)
+source("https://raw.githubusercontent.com/kbodwin/ShinyLabs/master/Scripts/makeStrings.R")
+
+correct <- "<font color='red'>Correct!</font><br><br>"
+incorrect <- "<font color='red'>Not quite right...</font><br><br>"
+congrats <- "<font color='red'>You did it!</font><br><br>"
