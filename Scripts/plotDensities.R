7e89f26e788766074e74547a991609deb9fdf8d0
Kelly Bodwin <kbodwin@calpoly.edu>
2018-10-16 14:50:41 -0700

    illustrate t dist

--- Scripts/plotDensities.R
+++ Scripts/plotDensities.R
@@ -75,3 +75,58 @@ illustrate_norm <- function(mu, sigma, x = NA, q = NA){
     p
   
 }
+
+
+illustrate_t <- function(df = 1000, t_score = NA, q = NA){
+  
+  df = as.integer(df)
+  t_score = as.numeric(t_score)
+  q = as.numeric(q)
+  
+  if(is.na(q) & is.na(x)){
+    t_score <- 10
+    q = 0
+  }else if(is.na(q)){
+    q = round(pt(t_score, df), 3)
+  }else{
+    t_score = qt(q, df)
+  }
+  
+  t_score = round(t_score, 2)
+  
+  ulim = max(t_score, 3) + 0.5
+  llim = min(t_score, -3) - 0.5
+  
+  p <- ggplot(data = data.frame(x = c(llim, ulim)), aes(x)) +
+    stat_function(fun = dt, args = list(df = df), lwd = 2) + 
+    stat_function(fun = dt, args = list(df = df),
+                  xlim = c(llim, t_score),
+                  geom = "area", fill = "blue", alpha = 0.5) +
+    geom_vline(xintercept = t_score, lwd = 1.5, col = "red") +
+    ylab("") + xlab("t scores") +
+    scale_y_continuous(breaks = NULL)
+  
+  
+  if(q > 0.1){
+    xpos <- t_score - (t_score-llim)/3
+    ypos <- dt(xpos, df = df)/3
+  }else{
+    xpos <- t_score - (t_score-llim)/2
+    ypos <- dnorm(xpos, df = df)*4
+  }
+  
+  
+  xbreaks = c((-3:3), t_score)
+  xcols <- c(rep("black", 7), "red")
+  
+  p <- p + 
+    annotate("text", x = xpos, y = ypos, label = paste0(q*100, "%"), size = 7, color = "red") +
+    annotate("text", x = 2, y = dt(0, df)*.75, label = "df = ", size = 7) +
+    annotate("text", x = 2.6, y = dt(0, df)*.75, label = df, size = 7, color = "red") +
+    scale_x_continuous(breaks = xbreaks, labels = xbreaks) + 
+    theme(axis.text.x = element_text(colour = xcols, size = c(rep(10, 7), 15)), panel.grid.minor = element_blank())
+  
+  
+  p
+  
+}
